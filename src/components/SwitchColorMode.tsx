9e711a54681b3141f363ce86d4408e2253520a53
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2022-11-20 10:23:54 -0300

    Switch para el cambio de mode 🌞🌚

---
+++ src/components/SwitchColorMode.tsx
@@ -0,0 +1,22 @@
+import { Typography, Switch } from '@mui/material';
+import { useColorMode, useSaveMode } from '../theme/hooks';
+
+const SwitchColorMode = () => {
+  const mode = useColorMode();
+  const saveMode = useSaveMode();
+
+  const setTheme = () =>
+    mode === 'light' ? saveMode('dark') : saveMode('light');
+
+  return (
+    <>
+      <Switch
+        color="primary"
+        onChange={setTheme}
+      />
+      <Typography>{`${mode} mode`}</Typography>
+    </>
+  );
+};
+
+export default SwitchColorMode;
