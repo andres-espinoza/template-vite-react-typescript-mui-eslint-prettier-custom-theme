086b657371904004ee321a38cebba30b83ff0f4d
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2022-11-20 14:05:57 -0300

    by Andy 🧙‍♂️✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Typography, ThemeProvider } from '@mui/material';
+import { Typography, ThemeProvider, Link } from '@mui/material';
 import IconStack from './components/IconStack';
 import SwitchColorMode from './components/SwitchColorMode';
 import MainContainer from './Layouts/MainContainer';
@@ -15,6 +15,15 @@ const App = () => {
         <Typography variant="body1">Hello World!</Typography>
         <SwitchColorMode />
         <IconStack />
+        <Link
+          href="https://www.linkedin.com/in/andres-espinoza-delgado-fullstack-developer-typescript-react-nodejs-express-mongodb/"
+          target="_blank"
+          sx={{
+            marginTop: 10,
+          }}
+        >
+          <Typography variant="body1">by Andy 🧙‍♂️</Typography>
+        </Link>
       </MainContainer>
     </ThemeProvider>
   );
